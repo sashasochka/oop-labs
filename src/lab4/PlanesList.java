481d4c616d4b02504b40f26e6901a72cccf58243
Oleksandr <sasha.sochka@gmail.com>
2013-11-03 14:43:30 +0200

    Add equals and hashCode implementation to PlanesList

--- src/lab4/PlanesList.java
+++ src/lab4/PlanesList.java
@@ -419,6 +419,31 @@ class PlanesList implements List<Plane> {
         return element;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof List))
+            return false;
+
+        ListIterator e1 = listIterator();
+        ListIterator e2 = ((List) o).listIterator();
+        while (e1.hasNext() && e2.hasNext()) {
+            Object o1 = e1.next();
+            Object o2 = e2.next();
+            if (!(o1==null ? o2==null : o1.equals(o2)))
+                return false;
+        }
+        return !(e1.hasNext() || e2.hasNext());
+    }
+
+    public int hashCode() {
+        int hashCode = 1;
+        for (Object e : this) {
+            hashCode = 31 * hashCode + (e == null ? 0 : e.hashCode());
+        }
+        return hashCode;
+    }
 
     /**
      * Node class represents a node in a doubly-linked list data structure
